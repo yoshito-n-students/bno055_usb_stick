99f2972153bfc3ad69c319e5ae22bf0bf1caba0f
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2016-11-14 11:24:39 +0900

    add bare files

---
+++ include/bno055_usb_stick/bno055_usb_stick.hpp
@@ -0,0 +1,16 @@
+#ifndef BNO055_USB_STICK_BNO055_USB_STICK_HPP
+#define BNO055_USB_STICK_BNO055_USB_STICK_HPP
+
+namespace bno055_usb_stick {
+
+class BNO055USBStick {
+  public:
+    BNO055USBStick() {}
+
+    virtual ~BNO055USBStick() {}
+
+  private:
+};
+}
+
+#endif // BNO055_USB_STICK_BNO055_USB_STICK_HPP
---
+++ src/bno055_usb_stick_node.cpp
@@ -0,0 +1,5 @@
+#include <bno055_usb_stick/bno055_usb_stick.hpp>
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
