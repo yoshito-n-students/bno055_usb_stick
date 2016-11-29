6342450afd959bddf5ac805c197b53f2404abdff
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2016-11-30 07:46:45 +0900

    add communication timeout and retry

--- include/bno055_usb_stick/bno055_usb_stick.hpp
+++ include/bno055_usb_stick/bno055_usb_stick.hpp
@@ -7,6 +7,7 @@
 #include <string>
 
 #include <ros/console.h>
+#include <ros/duration.h>
 #include <ros/names.h>
 #include <ros/param.h>
 
@@ -37,6 +38,7 @@ class BNO055USBStick {
     BNO055USBStick(boost::asio::io_service &asio_service, const Callback &callback,
                    const std::string &ns = "~")
         : port_(ros::param::param< std::string >(ros::names::append(ns, "port"), "/dev/ttyACM0")),
+          timeout_(ros::param::param(ros::names::append(ns, "timeout"), 1.)),
           mode_(ros::param::param< std::string >(ros::names::append(ns, "mode"), "ndof")),
           serial_(asio_service), timer_(asio_service), callback_(callback), decoder_(ns) {
         start();
@@ -46,6 +48,9 @@ class BNO055USBStick {
 
   private:
     void start() {
+        // stop the previous operaiton if there
+        stop();
+
         // setup the serial port
         try {
             serial_.open(port_);
@@ -57,7 +62,9 @@ class BNO055USBStick {
             serial_.set_option(Serial::stop_bits(Serial::stop_bits::one));
             serial_.set_option(Serial::character_size(8));
         } catch (const boost::system::system_error &error) {
+            // retry if something is wrong
             ROS_ERROR_STREAM("start: " << error.what());
+            restart();
             return;
         }
 
@@ -93,6 +100,7 @@ class BNO055USBStick {
     void startSendCommand() {
         if (commands_.empty()) {
             ROS_ERROR("startSendCommand: No command in the queue");
+            restart();
             return;
         }
 
@@ -101,16 +109,23 @@ class BNO055USBStick {
         boost::asio::async_write(serial_,
                                  boost::asio::buffer(command, Constants::getCommandLength(command)),
                                  boost::bind(&BNO055USBStick::handleSendCommand, this, _1, _2));
+
+        // schedule restarting in case of timeout
+        startWaitDeadline(&BNO055USBStick::restart);
     }
 
     void handleSendCommand(const boost::system::error_code &error, const std::size_t bytes) {
+        // cancel the timeout action
+        cancelWaitDeadline();
+
         if (error) {
             ROS_ERROR_STREAM("handleSendCommand: " << error.message());
+            restart();
             return;
         }
 
         // pop the top command from the queue
-        // dumpWritten("handleSendCommand: written: ");
+        // dumpWritten("handleSendCommand: written: ", bytes);
         commands_.pop_front();
 
         // trigger wait the response for the command
@@ -118,14 +133,22 @@ class BNO055USBStick {
     }
 
     void startWaitResponse() {
+        // trigger read a responce
         boost::asio::async_read_until(
             serial_, buffer_, ResponseCondition(),
             boost::bind(&BNO055USBStick::handleWaitResponse, this, _1, _2));
+
+        // schedule restarting in case of timeout
+        startWaitDeadline(&BNO055USBStick::restart);
     }
 
     void handleWaitResponse(const boost::system::error_code &error, const std::size_t bytes) {
+        // cancel the timeout action
+        cancelWaitDeadline();
+
         if (error) {
             ROS_ERROR_STREAM("handleWaitResponse: " << error.message());
+            restart();
             return;
         }
 
@@ -142,13 +165,21 @@ class BNO055USBStick {
     }
 
     void startWaitData() {
+        // trigger read a data
         boost::asio::async_read_until(serial_, buffer_, DataCondition(),
                                       boost::bind(&BNO055USBStick::handleWaitData, this, _1, _2));
+
+        // schedule restarting in case of timeout
+        startWaitDeadline(&BNO055USBStick::restart);
     }
 
     void handleWaitData(const boost::system::error_code &error, const std::size_t bytes) {
+        // cancel the timeout action
+        cancelWaitDeadline();
+
         if (error) {
             ROS_ERROR_STREAM("handleWaitData: " << error.message());
+            restart();
             return;
         }
 
@@ -169,12 +200,60 @@ class BNO055USBStick {
         startWaitData();
     }
 
-    void stop() {}
+    void stop() {
+        // finalize the serial port
+        try {
+            if (serial_.is_open()) {
+                serial_.close();
+            }
+        } catch (const boost::system::system_error &error) {
+            // just print the error because can do nothing further
+            ROS_ERROR_STREAM("stop: " << error.what());
+        }
+    }
 
-    void dumpWritten(const std::string &prefix) {
+    void restart() {
+        // schedule start a new operation
+        startWaitDeadline(&BNO055USBStick::start);
+    }
+
+    void startWaitDeadline(void (BNO055USBStick::*handler)()) {
+        timer_.expires_from_now(timeout_.toBoost());
+        timer_.async_wait(boost::bind(&BNO055USBStick::handleWaitDeadline, this, _1, handler));
+    }
+
+    void cancelWaitDeadline() { timer_.cancel(); }
+
+    void handleWaitDeadline(const boost::system::error_code &error,
+                            void (BNO055USBStick::*handler)()) {
+        // make sure this callback is called by a deadline expiration
+        if (error == boost::asio::error::operation_aborted) {
+            // ROS_INFO("handleWaitDeadline: the deadline disabled");
+            return;
+        } else if (error) {
+            ROS_ERROR_STREAM("handleWaitDeadline: " << error.message());
+            return;
+        }
+
+        // try to cancel all operations on the serial port
+        try {
+            if (serial_.is_open()) {
+                serial_.cancel();
+            }
+        } catch (const boost::system::system_error &error_on_cancel) {
+            ROS_ERROR_STREAM("handleWaitDeadline: " << error_on_cancel.what());
+        }
+
+        // execute the given handler
+        if (handler) {
+            (this->*handler)();
+        }
+    }
+
+    void dumpWritten(const std::string &prefix, const std::size_t bytes) {
         std::ostringstream oss;
         const boost::uint8_t *begin(commands_.front());
-        const boost::uint8_t *end(begin + Constants::getCommandLength(commands_.front()));
+        const boost::uint8_t *end(begin + bytes);
         for (const boost::uint8_t *c = begin; c != end; ++c) {
             oss << "0x" << std::setw(2) << std::setfill('0') << std::hex << int(*c) << " ";
         }
@@ -195,6 +274,7 @@ class BNO055USBStick {
   private:
     // parameters
     const std::string port_;
+    const ros::Duration timeout_;
     const std::string mode_;
 
     // buffers
